109dcd7d551b8a6ce3e0aebf1b3d919d58531562
zhengx55 <williamzhxin@gmail.com>
2024-10-26 17:45:58 +0800

    Refactor next.config.ts and package.json
    - Update next.config.ts to include new image and experimental configurations.
    - Remove unused SVG files from the public directory.
    - Update package.json to remove the "turbopack" flag in the "dev" script.
    - Add local font imports and Toaster component to layout.tsx.
    - Update metadata in layout.tsx.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
 };
 
 export default nextConfig;
